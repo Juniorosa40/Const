f2be76fd901821f924c01b5df87b1b434a36dc84
Junior Rosa <acsrosajunior@gmail.com>
2022-11-01 16:06:25 -0300

    Podemos alterar os valores constantes criando novas funções com valores ja existentes tornando assim nosso codigo sempre limpo e de facil entendimento.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const primeiroNumero = 8;
 const segundoNumero = 10;
 const terceiroNumero = 7;
 const resultado = primeiroNumero * segundoNumero + terceiroNumero;
-console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
